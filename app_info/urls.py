a919862b0dea882e2046b7d7233803a628329395
yubrajthapa <yubrajthapa742@gmail.com>
2022-07-13 22:00:41 +0545

    Bootstrap

---
+++ app_info/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("users/dashboard/", views.user_dashboard, name="user.dashboard"),
+]
--- app_info/views.py
+++ app_info/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def user_dashboard(request):
+    return render(request, "users/dashboard.html")
