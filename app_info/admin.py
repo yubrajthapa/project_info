465d56fbbd7d363662956782012a4d53e3829a6f
yubrajthapa <yubrajthapa742@gmail.com>
2022-07-21 22:43:48 +0545

    AdminPannelDisplay

--- app_info/admin.py
+++ app_info/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+#To import selectively
+#from . models import StudentDetail, AcademicDetail, TrainingDetail
+
+from .models import *
 
 # Register your models here.
+# This will display these fields in admin pannel
+# StudentsDetailssAdmin is independent not inherited from anywhere.
+class StudentDetailssAdmin(admin.ModelAdmin):
+    list_display= ("first_name", "middle_name", "last_name", "email", "contact")
+    list_filter = ("first_name", "email", "contact")
+    search_fields = ("first_name", "middle_name", "last_name", "email", "contact")
+
+admin.site.register(StudentDetail, StudentDetailssAdmin)
+admin.site.register(AcademicDetail)
+admin.site.register(TrainingDetail)
